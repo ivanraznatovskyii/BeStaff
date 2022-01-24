4391df6cd68782e9207799a4727d1f718d25f0d7
Ivan <raznatovskyii.work@gmail.com>
2022-01-24 08:31:24 +0200

    Added home-page, search-page and not-found page

--- client/src/app/app-routing.module.ts
+++ client/src/app/app-routing.module.ts
@@ -1,7 +1,12 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { HomePageComponent } from './components/pages/home-page/home-page.component';
+import { NotFoundComponent } from './components/pages/not-found/not-found.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: '', component: HomePageComponent },
+  { path: '**', component: NotFoundComponent }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
