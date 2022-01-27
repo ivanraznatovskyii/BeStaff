5e94648b1a99340f6c72bf56f75570778e63643e
Ivan <raznatovskyii.work@gmail.com>
2022-01-27 10:47:12 +0200

    Added dev's block

--- client/src/app/components/available-developers/available-developers.component.ts
+++ client/src/app/components/available-developers/available-developers.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, ElementRef, OnInit, TemplateRef, ViewChild } from '@angular/core';
 import { Observable, Observer } from 'rxjs';
 
 @Component({
@@ -9,14 +9,42 @@ import { Observable, Observer } from 'rxjs';
 export class AvailableDevelopersComponent implements OnInit {
 
   asyncTabs: Observable<any[]>;
+  username: string = 'User Name';
+  position: string = 'Java Developer';
+  specialization: string = '';
+  experience: string = '';
+  devs: any = [
+    {
+      username: 'User Name',
+      position: 'Java Developer',
+      specialization: 'Lorem, ipsum dolor sit amet consectetur adipisicing elit. Dicta perspiciatis, officiis amet voluptatum voluptate explicabo.',
+      experience: 'Lorem, ipsum dolor sit amet consectetur adipisicing elit. Dicta perspiciatis, officiis amet voluptatum voluptate explicabo.',
+    },
+    {
+      username: 'User Name',
+      position: 'Java Developer',
+      specialization: 'Lorem, ipsum dolor sit amet consectetur adipisicing elit. Dicta perspiciatis, officiis amet voluptatum voluptate explicabo.',
+      experience: 'Lorem, ipsum dolor sit amet consectetur adipisicing elit. Dicta perspiciatis, officiis amet voluptatum voluptate explicabo.',
+    },
+    {
+      username: 'User Name',
+      position: 'Java Developer',
+      specialization: 'Lorem, ipsum dolor sit amet consectetur adipisicing elit. Dicta perspiciatis, officiis amet voluptatum voluptate explicabo.',
+      experience: 'Lorem, ipsum dolor sit amet consectetur adipisicing elit. Dicta perspiciatis, officiis amet voluptatum voluptate explicabo.',
+    }
+  ];
+
+  @ViewChild('devCard') devCard!: TemplateRef<any>;
 
   constructor() {
     this.asyncTabs = new Observable((observer: Observer<any[]>) => {
       setTimeout(() => {
         observer.next([
-          {label: 'First', content: 'Content 1'},
-          {label: 'Second', content: 'Content 2'},
-          {label: 'Third', content: 'Content 3'},
+          {label: 'Frontend', template: this.devCard },
+          {label: 'Backend', content: 'Content 2'},
+          {label: 'FullStack', content: 'Content 3'},
+          {label: 'QA', content: 'Content 3'},
+          {label: 'DevOps', content: 'Content 3'},
         ]);
       }, 1000);
     });
