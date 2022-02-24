0baa8197904ac9b40e17f7271dfefaaa0695c224
Ivan <ivan.zf.netc@gmail.com>
2022-02-24 11:21:54 +0200

    .

--- routes/registration.js
+++
@@ -1,9 +0,0 @@
-const express = require('express');
-const controller = require('../controllers/registration.controller');
-const router = express.Router();
-
-
-router.get('/positions', controller.getPositions);
-
-
-module.exports = router;
